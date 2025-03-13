575db62d5082161a238134e37662f15b09e9974a
김동엽 <yeophyeong@gimdong-yeob-ui-MacBookAir.local>
2025-03-13 21:47:29 +0900

    이력서 업로드 수정4

--- jssgpt_back/jssgpt_project/jssgpt_project/settings.py
+++ jssgpt_back/jssgpt_project/jssgpt_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jssgpt.com', 'www.jssgpt.com', "'jssgpt.com'" '223.130.159.46', '127.0.0.1']
+ALLOWED_HOSTS = ['jssgpt.com', 'www.jssgpt.com', "'jssgpt.com'", '223.130.159.46', '127.0.0.1']
 
 
 # Application definition
