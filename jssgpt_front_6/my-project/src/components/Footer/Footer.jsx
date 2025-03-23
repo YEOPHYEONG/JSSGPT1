8583cee4b4568d13c60137fc14637ae8fdca5936
김동엽 <yeophyeong@gimdong-yeob-ui-MacBookAir.local>
2025-03-24 01:41:41 +0900

    3.23 프론트 디자인 수정 3

--- jssgpt_front_6/my-project/src/components/Footer/Footer.jsx
+++ jssgpt_front_6/my-project/src/components/Footer/Footer.jsx
@@ -5,7 +5,10 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 JSSGPT. All rights reserved.</p>
+      <p>
+      <span className={styles.sparkle}>📭 피드백 대환영! 문의 및 불편사항: geuloing@gmail.com </span> <br />
+      © 2025 JSSGPT. All rights reserved. 
+      </p>
     </footer>
   );
 }
